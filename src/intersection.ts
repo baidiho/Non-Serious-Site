d402e087976e94f4b23c3022c3239cb9d95e6671
Ihor <ygrik10@gmail.com>
2022-12-28 15:13:13 +0200

    Add intersection.ts and style for intersect

--- src/intersection.ts
+++ src/intersection.ts
@@ -1,16 +1,26 @@
-// настройки
-let options = {
-	threshold: [0.8]
-};
+function onLoadObserver(): void {
+  // настройки
+  let options = {
+    threshold: [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1],
+  };
 
-// функция обратного вызова
-let callback = function (entries: any) {
-	console.log(entries[0].target);
-};
+  //Callback function
+  let callback = function (entries: any) {
+    if (entries[0].isIntersecting) {
+      console.log(entries[0]);
+      entries[0].target.classList.add("topic-description-animated");
+      observer.unobserve(entries[0].target);
+    }
+  };
 
-// наблюдатель
-let observer = new IntersectionObserver(callback, options);
-const observableElement: any = document.querySelectorAll(".topic-container").forEach((element) => {
-	observer.observe(element);
-});
-// console.log(observableElement);
+  // наблюдатель
+  let observer = new IntersectionObserver(callback, options);
+
+  const observableElement: any = document.querySelectorAll(".topic-description");
+
+  observableElement.forEach((element: any) => {
+    observer.observe(element);
+  });
+}
+window.addEventListener("load", () => onLoadObserver());
+// // console.log(observableElement);
