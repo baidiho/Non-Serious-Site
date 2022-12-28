9e9fcd7db28cf3d1d57ae9c428d682db5ee2d33c
Ihor <ygrik10@gmail.com>
2022-12-28 20:39:22 +0200

    Animations done

--- src/button-animation.ts
+++ src/button-animation.ts
@@ -1,9 +1,15 @@
-let button: any = document.querySelector(".topic-demonstration-button");
+let button: any = document.querySelector(".button-animation");
+let cubic = document.querySelectorAll(".cubic");
 function animationButton(e: any): void {
 	let parent = e.target.parentNode;
 	parent.classList.add("hiding");
-
 	button.style.display = "none";
+	button.removeEventListener("click", animationButton);
+	parent.onanimationend = () => {
+		cubic.forEach((element) => {
+			element.classList.add("cubic-visible");
+		});
+	};
 }
 button.addEventListener("click", animationButton, true);
 console.log(document.querySelectorAll(".topic-wrapper"));
--- src/typeWritter.ts
+++ src/typeWritter.ts
@@ -2,8 +2,8 @@ export default function typewriterScript() {
 	const elementText = document.querySelector(".text-wrapper p");
 	const mockText =
 		"My name is Ihor. If you are looking this site now, you propably have seen my CV or GitHub account. " +
-		"This site was made for fun, exprerience and portfolio. I've tried to use all junior's features such as" +
-		" adaptive and responsible design, Intercetion, animations and etc.I've add description with what feature was used there to all sections.";
+		"This site was made for fun, exprerience and portfolio. I've tried to show that i know something features." +
+		"Give me a job.";
 	let i: number = 0;
 	function recursion(): any {
 		if (i > mockText.length) {
@@ -11,7 +11,7 @@ export default function typewriterScript() {
 		} else {
 			elementText.innerHTML = `${mockText.slice(0, i)}|`;
 			i++;
-			return setTimeout(() => recursion(), 15);
+			return setTimeout(() => recursion(), 12);
 		}
 	}
 	recursion();
