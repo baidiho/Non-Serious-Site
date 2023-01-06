9c0637f4c327f1e40277eb0bd689935c32efbaae
Ihor <ygrik10@gmail.com>
2023-01-06 12:24:26 +0200

    progress with drag

--- src/dragScript.ts
+++ src/dragScript.ts
@@ -1,35 +1,45 @@
-const draggableObj = document.querySelectorAll(".grid-item");
-
-class dragObj {
-  private target: any;
+let startElement: any;
+class DragObj {
   onDragStart(event: any) {
-    event.dataTransfer.setData("text/plain", event.target.textContent);
-    this.target = event.target;
-    console.log(this.target);
-    console.log("start");
+    event.dataTransfer.setData("text/html", event.target.innerHTML);
+    event.dataTransfer.effectAllowed = "move";
+    startElement = event.target;
+    event.target.classList.add("grid-item-drag");
+  }
+  onDragEnter(event: any) {
+    event.target.classList.add("grid-item-over");
+  }
+  onDragLeave(event: any) {
+    event.target.classList.remove("grid-item-over");
   }
   onDragEnd(event: any) {
-    // this.target.textContent = "";
-    // this.target = null;
-    console.log("end");
-    console.log(this.target);
-  }
-  onDrop(e: any) {
-    if (e.target != this.target) {
-      const data = e.dataTransfer.getData("text");
-      e.target.textContent = data;
-      console.log(this.target);
-      console.log("drop");
+    if (event.target != startElement) {
+      event.dataTransfer.clearData();
+      startElement.classList.remove("grid-item-drag");
     }
+    startElement.classList.remove("grid-item-drag");
+  }
+  onDrop(event: any) {
+    if (event.target != startElement) {
+      startElement.innerHTML = event.target.innerHTML;
+      event.target.innerHTML = event.dataTransfer.getData("text/html");
+      event.target.classList.remove("grid-item-over");
+    }
+    event.target.classList.remove("grid-item-over");
+  }
+  onDragOver(e: any) {
+    e.preventDefault();
   }
 }
-draggableObj.forEach((element) => {
-  const dragInst = new dragObj();
 
+const dragInst = new DragObj();
+const draggableObj = document.querySelectorAll(".grid-item");
+
+draggableObj.forEach((element) => {
   element.addEventListener("dragstart", dragInst.onDragStart);
+  element.addEventListener("dragenter", dragInst.onDragEnter);
+  element.addEventListener("dragleave", dragInst.onDragLeave);
   element.addEventListener("drop", dragInst.onDrop);
-  element.addEventListener("dragover", (event: any) => {
-    event.preventDefault();
-  });
   element.addEventListener("dragend", dragInst.onDragEnd);
+  element.addEventListener("dragover", dragInst.onDragOver);
 });
