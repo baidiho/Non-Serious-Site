00132f025cc152ba27e43f1defece6a9542590a2
Ihor <ygrik10@gmail.com>
2023-01-17 20:51:51 +0200

    Some fix

--- src/form.ts
+++ src/form.ts
@@ -1,78 +1,75 @@
-const form: any = document.querySelector('form[name="my"]');
-const elements: any = form.elements;
-const storageWpapper = document.querySelector(".storage-info");
-const refreshButton = document.querySelector(".clear");
+(function () {
+	const form: any = document.querySelector('form[name="my"]');
 
-function formSumbit(e: Event) {
-	e.preventDefault();
-	Array.from(elements).forEach((element: any) => {
-		if (element.type == "text") {
-			window.localStorage.setItem(`${element.name}`, `${element.value}`);
-		}
-	});
-	storageHandler();
-}
+	const storageWpapper = document.querySelector(".storage-info");
+	const refreshButton = document.querySelector(".clear");
 
-function storageHandler() {
-	if (window.localStorage.length == 0) {
-		storageWpapper.classList.remove("storage-info-visible");
-		return;
-	} else {
-		const paragraphs = document.querySelectorAll(".storage-info p");
-		paragraphs.forEach((para: HTMLElement) => {
-			para.textContent = window.localStorage.getItem(para.dataset.name);
+	function formSumbit(e: Event) {
+		e.preventDefault();
+		Array.from(form.elements).forEach((element: any) => {
+			if (element.type == "text") {
+				window.localStorage.setItem(`${element.name}`, `${element.value}`);
+			}
 		});
-		storageWpapper.classList.add("storage-info-visible");
-		refreshButton.addEventListener("click", clearStorage);
+		storageHandler();
 	}
-}
-function clearStorage() {
-	window.localStorage.clear();
-	refreshButton.removeEventListener("click", clearStorage);
-	storageHandler();
-}
 
-window.addEventListener("load", storageHandler);
-form.addEventListener("submit", formSumbit);
-async function start() {
-	return await Promise.resolve("asyns is working");
-}
-start().then(console.log);
+	function storageHandler(): void {
+		if (window.localStorage.length == 0) {
+			storageWpapper.classList.remove("storage-info-visible");
+		} else {
+			const paragraphs = document.querySelectorAll(".storage-info p");
+			paragraphs.forEach((para: HTMLElement) => {
+				para.textContent = window.localStorage.getItem(para.dataset.name);
+			});
+			storageWpapper.classList.add("storage-info-visible");
+			refreshButton.addEventListener("click", clearStorage);
+		}
+	}
+	function clearStorage() {
+		window.localStorage.clear();
+		refreshButton.removeEventListener("click", clearStorage);
+		storageHandler();
+	}
 
-// const form: any = document.querySelector('form[name="my"]');
-// const elements: any = form.elements;
-// const refreshButton = document.querySelector(".clear");
-// function formSumbit(e: Event) {
-// 	e.preventDefault();
-// 	window.localStorage.setItem("firstname", elements.firstname.value);
-// 	window.localStorage.setItem("lastname", elements.lastname.value);
-// 	storageHandler();
-// }
+	window.addEventListener("load", storageHandler);
+	form.addEventListener("submit", formSumbit);
+})();
 
-// function storageHandler() {
-// 	if (window.localStorage.length == 0) {
-// 		console.log("i am empty");
-// 		const asdas = document.querySelector(".storage-info");
-// 		asdas.classList.remove("storage-info-visible");
-// 		return;
-// 	} else {
-// 		const paragraphs = document.querySelectorAll(".storage-info p");
-// 		paragraphs.forEach((para: HTMLElement) => {
-// 			para.textContent = window.localStorage.getItem(para.dataset.name);
+// class StorageHandler {
+// 	form: any = document.querySelector('form[name="my"]');
+// 	elements: any = this.form.elements;
+// 	storageWpapper = document.querySelector(".storage-info");
+// 	refreshButton = document.querySelector(".clear");
+
+// 	storageHandler(): void {
+// 		if (window.localStorage.length == 0) {
+// 			this.storageWpapper.classList.remove("storage-info-visible");
+// 		} else {
+// 			const paragraphs = document.querySelectorAll(".storage-info p");
+// 			paragraphs.forEach((para: HTMLElement) => {
+// 				para.textContent = window.localStorage.getItem(para.dataset.name);
+// 			});
+// 			this.storageWpapper.classList.add("storage-info-visible");
+// 			this.refreshButton.addEventListener("click", this.clearStorage);
+// 		}
+// 	}
+// 	formSumbit(e: Event) {
+// 		e.preventDefault();
+// 		Array.from(this.elements).forEach((element: any) => {
+// 			if (element.type == "text") {
+// 				window.localStorage.setItem(`${element.name}`, `${element.value}`);
+// 			}
 // 		});
-// 		const asdas = document.querySelector(".storage-info");
-// 		asdas.classList.add("storage-info-visible");
-// 		refreshButton.addEventListener("click", clearStorage);
+// 		this.storageHandler();
+// 	}
+// 	clearStorage() {
+// 		window.localStorage.clear();
+// 		this.refreshButton.removeEventListener("click", this.clearStorage);
+// 		console.log("clear is work");
+// 		this.storageHandler();
 // 	}
 // }
-// function clearStorage() {
-// 	window.localStorage.clear();
-// 	refreshButton.removeEventListener("click", clearStorage);
-// 	storageHandler();
-// }
-
-// window.addEventListener("load", storageHandler);
-// form.addEventListener("submit", formSumbit);
-// class StorageForm {
-
-// }
+// const storageExemplar = new StorageHandler();
+// window.addEventListener("load", storageExemplar.storageHandler);
+// storageExemplar.form.addEventListener("submit", storageExemplar.formSumbit);
