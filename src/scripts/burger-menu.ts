46ceeba53f20e5e4228b4318895f7ff5305a8226
Ihor <ygrik10@gmail.com>
2023-02-16 15:32:26 +0200

    asdasd

--- src/scripts/burger-menu.ts
+++ src/scripts/burger-menu.ts
@@ -1,14 +1,16 @@
 let burgerButton = document.querySelector(".burger-menu");
 let navList = document.querySelector(".nav-list");
 let navBar = document.querySelector(".nav-bar");
+let modal = document.querySelector(".modal-menu");
 console.log(burgerButton);
 burgerButton.addEventListener("click", burgerMenuToggle);
 function burgerMenuToggle() {
   burgerButton.classList.toggle("pressed");
-  // navBar.classList.toggle("active");
-  if (navBar.classList.contains("close")) {
-    navBar.classList.replace("close", "open");
-  } else navBar.classList.replace("open", "close");
-  // navBar.classList.replace("open", "active");
-  // console.log(navBar.classList);
+  modal.classList.toggle('modal-open')
+  // // navBar.classList.toggle("active");
+  // if (navBar.classList.contains("close")) {
+  //   navBar.classList.replace("close", "open");
+  // } else navBar.classList.replace("open", "close");
+  // // navBar.classList.replace("open", "active");
+  // // console.log(navBar.classList);
 }
