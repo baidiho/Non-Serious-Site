868ecd3a85330489b978fd5e5d5593509a9d5ec4
Ihor <ygrik10@gmail.com>
2023-02-23 14:42:00 +0200

    Pagination and render almost done

--- src/scripts/posts-model.ts
+++ src/scripts/posts-model.ts
@@ -1,31 +1,12 @@
-import Post from "./types";
-let base = new URL("https://jsonplaceholder.typicode.com");
-let responsePosts: Array<Post>;
-export async function requestData(url: URL, headers?: Object) {
+export async function requestToServer(url: URL, options?: Object) {
   try {
     let request = await fetch(url);
     let response = await request.json();
-    return request.ok;
+    return response;
   } catch (err) {
-    console.log("Handling-erorrr");
+    console.log("Error is throw");
+    throw err;
   }
-  await console.log("Error is gone");
 }
-// async function postRequest(url: URL, headers?: Object) {
-//   try {
-//     let request = await fetch(url, headers);
-//     responsePosts = await request.json();
-//     console.log(responsePosts);
-//     responsePosts.forEach((post) => {
-//       renderPost(post);
-//     });
-//   } catch (err) {
-//     console.log(err);
-//   }
-// }
-// postRequest(new URL("/posts", base));
-// if (responsePosts) {
-//   console.log(responsePosts);
-// } else {
-//   console.log("Nothing to show");
-// }
+
+export async function requestComments() {}
--- src/scripts/posts-view.ts
+++ src/scripts/posts-view.ts
@@ -1,14 +1,43 @@
-import Post from "./types";
-export default function renderPost(post: Post) {
-  if (post) {
-    let postElement = document.createElement("div");
-    postElement.classList.add("post");
-    let postElementDescription = document.createElement("h1");
-    postElementDescription.textContent = post.title;
-    let postElementBody = document.createElement("p");
-    postElementBody.textContent = post.body;
-    postElement.appendChild(postElementDescription);
-    postElement.appendChild(postElementBody);
-    document.querySelector(".topic-content-async").appendChild(postElement);
+import { Post } from "./types";
+import { switchPages, moveToPost } from "./posts-controller";
+
+
+export function renderPosts(posts: Array<Post> | null, parentElement: Element, pageNumber: number, postsLimit: number) {
+  if (posts) {
+    parentElement.replaceChildren();
+    for (let i = (pageNumber - 1) * postsLimit; i <= pageNumber * postsLimit - 1; i++) {
+      let postElement = document.createElement("div");
+      postElement.classList.add("post");
+      let postElementDescription = document.createElement("h1");
+      postElementDescription.textContent = `Post#${posts[i].id} ${posts[i].title}`;
+      let postElementBody = document.createElement("p");
+      postElementBody.textContent = posts[i].body;
+      postElement.appendChild(postElementDescription);
+      postElement.appendChild(postElementBody);
+      parentElement.appendChild(postElement);
+      postElement.addEventListener("click", ()=>{moveToPost(posts[i].id)});
+    }
   }
 }
+export function renderError(parentElement: Element) {
+  parentElement.replaceChildren();
+  let errorMessage = document.createElement("p");
+  errorMessage.textContent = "Error in request. I am sorry";
+  parentElement.appendChild(errorMessage);
+}
+
+export function renderPageButtons(numberOfPages: number, activePage: number = 1, parentElement: Element) {
+  parentElement.replaceChildren();
+  for (let i = 0; i < numberOfPages; i++) {
+    let pageButton = document.createElement("div");
+    pageButton.classList.add("page-button");
+    if (activePage == i + 1) {
+      pageButton.classList.add("page-button-active");
+    }
+    pageButton.textContent = `${i + 1}`;
+    pageButton.dataset.indexNumber = `${i + 1}`;
+    parentElement.appendChild(pageButton);
+    pageButton.addEventListener("click", switchPages);
+  }
+}
+export function renderPostWithComments() {}
