e39db294708b5c708fe8f50564581f2ab2b546cd
Ihor <ygrik10@gmail.com>
2023-03-03 12:00:36 +0200

    Finally have made view class

--- src/scripts/post-script.ts
+++ src/scripts/post-script.ts
@@ -1,38 +1,14 @@
 export class PostController {
-	public baseUrl = new URL("https://jsonplaceholder.typicode.com");
+  public baseUrl = new URL("https://jsonplaceholder.typicode.com");
 
-	async requestToServer(url?: URL, options?: Object) {
-		try {
-			let request = await fetch(new URL("https://jsonplaceholder.typicode.com/posts")); // Need the solution
-			let response = await request.json();
-			return response;
-		} catch (err) {
-			console.log("Error is throw");
-			throw err;
-		}
-	}
-	// async pageRequest(options: PostControllerData) {
-	// 	try {
-	// 		options.arrayOfPosts = await requestToServer(new URL("/posts", options.baseUrl));
-	// 		options.numberOfPages = Math.ceil(options.arrayOfPosts.length / options.numberOfPostsOnPage);
-	// 		options.lastPageClicked = 1;
-	// 	} catch (err) {
-	// 		console.log("Error in controller");
-	// 		console.log(err);
-	// 		// renderError(options.mainContainer);
-	// 	}
-	// }
-
-	// async refreshPage(mainContainer: Element, typeOfPage: string): Promise<void> {
-	// 	if (typeOfPage == "main") {
-	// 		mainContainer.replaceChildren();
-	// 		this.view.renderFunctionalButton(typeOfPage, this.dataForPostTopic);
-	// 		this.view.renderPosts();
-	// 		this.view.renderPageNumberButtons();
-	// 	} else {
-	// 		// this.view.renderFunctionalButton(typeOfPage);
-	// 		// this.view.renderSinglePost();
-	// 		// this.view.renderComments();
-	// 	}
-	// }
+  async requestToServer(modifier: string) {
+    try {
+      let request = await fetch(new URL(modifier, this.baseUrl));
+      let response = await request.json();
+      return response;
+    } catch (err) {
+      console.log("Error is throw");
+      throw err;
+    }
+  }
 }
--- src/scripts/posts-view.ts
+++ src/scripts/posts-view.ts
@@ -1,213 +1,170 @@
 import { Post, Comments, PostControllerData, ViewRenderData } from "./types";
-import { switchPages, moveToPost } from "./posts-controller";
+
 import { PostController } from "./post-script";
 
-// function renderSinglePost(post: Post, parentElement: Element, callback?: Function) {
-// 	let postElement = document.createElement("div");
-// 	postElement.classList.add("post");
-// 	let postElementDescription = document.createElement("h1");
-// 	postElementDescription.textContent = `Post#${post.id} ${post.title}`;
-// 	let postElementBody = document.createElement("p");
-// 	postElementBody.textContent = post.body;
-// 	postElement.append(postElementDescription, postElementBody);
-// 	parentElement.appendChild(postElement);
-// 	if (callback) {
-// 		postElement.addEventListener("click", () => {
-// 			callback(post);
-// 		});
-// 	}
-// }
-// export function renderPosts(
-// 	posts: Array<Post> | Post,
-// 	parentElement: Element,
-// 	pageNumber: number,
-// 	postsLimit: number,
-// 	callback?: Function
-// ) {
-// 	parentElement.replaceChildren();
-// 	let postContainer = document.createElement("div");
-// 	postContainer.classList.add("posts-container");
-// 	if (Array.isArray(posts)) {
-// 		for (let i = (pageNumber - 1) * postsLimit; i <= pageNumber * postsLimit - 1; i++) {
-// 			renderSinglePost(posts[i], postContainer, callback);
-// 		}
-// 		parentElement.appendChild(postContainer);
-// 	} else {
-// 		console.log("I am here ");
-
-// 		renderSinglePost(posts, postContainer);
-// 		parentElement.appendChild(postContainer);
-// 		return postContainer;
-// 	}
-// }
-
-// export function renderError(parentElement: Element) {
-// 	parentElement.replaceChildren();
-// 	let errorMessage = document.createElement("p");
-// 	errorMessage.textContent = "Error in request. I am sorry";
-// 	parentElement.appendChild(errorMessage);
-// }
-
-// export function renderPageButtons(numberOfPages: number, activePage: number = 1, parentElement: Element) {
-// 	let buttonContainer = document.createElement("div");
-// 	buttonContainer.classList.add("page-button-container");
-
-// 	for (let i = 0; i < numberOfPages; i++) {
-// 		let pageButton = document.createElement("div");
-// 		pageButton.classList.add("page-button");
-// 		if (activePage == i + 1) {
-// 			pageButton.classList.add("page-button-active");
-// 		}
-
-// 		pageButton.textContent = `${i + 1}`;
-// 		pageButton.dataset.indexNumber = `${i + 1}`;
-// 		buttonContainer.appendChild(pageButton);
-// 		pageButton.addEventListener("click", switchPages);
-// 	}
-// 	parentElement.appendChild(buttonContainer);
-// }
-
-// /* This is for single post page for a while */
-// export function renderPostWithComments(post: Post, parentElement: Element) {
-// 	parentElement.replaceChildren();
-// 	let postElement = document.createElement("div");
-// 	postElement.classList.add("post");
-// 	let postElementDescription = document.createElement("h1");
-// 	postElementDescription.textContent = `Post#${post.id} ${post.title}`;
-// 	let postElementBody = document.createElement("p");
-// 	postElementBody.textContent = post.body;
-// 	let buttonBack = document.createElement("button");
-// 	buttonBack.classList.add("back-button");
-// 	buttonBack.textContent = "Back";
-// 	postElement.appendChild(buttonBack);
-// 	postElement.appendChild(postElementDescription);
-// 	postElement.appendChild(postElementBody);
-// 	parentElement.appendChild(postElement);
-// }
-
-// export function renderComments(comments: Array<Comments>, parentElement: Element) {
-// 	if (comments) {
-// 		for (let i = 0; i <= comments.length - 1; i++) {
-// 			let commentElement = document.createElement("div");
-// 			commentElement.classList.add("comment");
-// 			let commentNumber = document.createElement("h1");
-// 			commentNumber.textContent = `Comment#${i + 1}`;
-// 			let commentatorName = document.createElement("h2");
-// 			commentatorName.textContent = comments[i].name;
-// 			let commentatorEmail = document.createElement("h2");
-// 			commentatorEmail.textContent = comments[i].email;
-// 			let commentText = document.createElement("p");
-// 			commentText.textContent = comments[i].body;
-// 			commentElement.appendChild(commentNumber);
-// 			commentElement.appendChild(commentatorEmail);
-// 			commentElement.appendChild(commentText);
-// 			parentElement.appendChild(commentElement);
-// 		}
-// 	}
-// }
-/**************************************************************************************** */
 export class PostView {
-	public controller: PostController;
-	constructor() {
-		this.controller = new PostController();
-	}
-	public renderData: ViewRenderData = {
-		mainContainer: document.querySelector(".topic-content-async"),
-		numberOfPostsOnPerPage: 5,
-		button: document.querySelector(".request"),
-		lastPageClicked: 1
-	};
+  public controller: PostController;
+  public this: Object;
+  constructor() {
+    this.controller = new PostController();
+    this.loadDataFromServer = this.loadDataFromServer.bind(this);
+    this.renderMainPage = this.renderMainPage.bind(this);
+    this.renderFunctionalButton = this.renderFunctionalButton.bind(this);
+    this.renderPosts = this.renderPosts.bind(this);
+    this.renderPageNumberButtons = this.renderPageNumberButtons.bind(this);
+    this.switchPage = this.switchPage.bind(this);
+    this.renderPostWithComments = this.renderPostWithComments.bind(this);
+    this.renderPostWithComments = this.renderPostWithComments.bind(this);
+    this.renderSinglePost = this.renderSinglePost.bind(this);
+  }
 
-	async loadDataFromServer() {
-		this.renderData.arraysOfPosts = await this.controller.requestToServer();
-		this.renderData.numberOfPages = Math.ceil(
-			this.renderData.arraysOfPosts.length / this.renderData.numberOfPostsOnPerPage
-		);
-		this.renderMainPage(this.renderData);
-	}
-	renderMainPage(options: ViewRenderData) {
-		options.mainContainer.replaceChildren();
-		this.renderFunctionalButton("main", this.renderData.mainContainer);
-		this.renderPosts(options, this.renderPostWithComments);
-		this.renderPageNumberButtons(options);
-	}
-	renderFunctionalButton(typeOfPage: string, container: Element, callback?: EventListener) {
-		const button = document.createElement("button");
-		if (typeOfPage == "main") {
-			button.classList.add("navigation-button", "request");
-			button.textContent = "Load posts";
-		} else {
-			button.classList.add("navigation-button", "back");
-			button.textContent = "Return";
-		}
-		container.appendChild(button);
-		if (callback) {
-			button.addEventListener("click", callback, { once: true });
-		}
-	}
+  public renderData: ViewRenderData = {
+    mainContainer: document.querySelector(".topic-content-async"),
+    numberOfPostsOnPerPage: 5,
+    button: document.querySelector(".request"),
+    lastPageClicked: 1,
+  };
 
-	renderPosts(options: ViewRenderData, callback?: Function) {
-		let postContainer = document.createElement("div");
-		postContainer.classList.add("posts-container");
-		for (
-			let i = (options.lastPageClicked - 1) * options.numberOfPostsOnPerPage;
-			i <= options.lastPageClicked * options.numberOfPostsOnPerPage - 1;
-			i++
-		) {
-			this.renderSinglePost(options.arraysOfPosts[i], postContainer, callback);
-		}
-		options.mainContainer.appendChild(postContainer);
-	}
-	renderPageNumberButtons(options: ViewRenderData) {
-		let buttonContainer = document.createElement("div");
-		buttonContainer.classList.add("page-button-container");
-		console.log("I ma render buttomn");
-		console.log(options.numberOfPages);
-		for (let i = 0; i < options.numberOfPages; i++) {
-			console.log("Some text in cycle");
+  async loadDataFromServer() {
+    try {
+      this.renderData.arraysOfPosts = await this.controller.requestToServer("/posts");
+      this.renderData.numberOfPages = Math.ceil(
+        this.renderData.arraysOfPosts.length / this.renderData.numberOfPostsOnPerPage
+      );
+      this.renderMainPage(this.renderData);
+    } catch (err) {
+      this.renderError(this.renderData);
+    }
+  }
+  /*Main page with all posts */
+  renderMainPage(options: ViewRenderData) {
+    options.mainContainer.replaceChildren();
+    this.renderFunctionalButton("main", this.renderData.mainContainer, this.loadDataFromServer);
+    this.renderPosts(options, this.renderPostWithComments);
+    this.renderPageNumberButtons(options);
+  }
+  renderFunctionalButton(typeOfPage: string, container: Element, callback?: EventListener) {
+    const button = document.createElement("button");
+    if (typeOfPage == "main") {
+      button.classList.add("navigation-button", "request");
+      button.textContent = "Load ";
+    } else {
+      button.classList.add("navigation-button", "back");
+      button.textContent = "Return";
+    }
+    container.appendChild(button);
+    if (callback) {
+      button.addEventListener("click", callback, { once: true });
+    }
+  }
 
-			let pageButton = document.createElement("div");
-			pageButton.classList.add("page-button");
-			if (options.lastPageClicked == i + 1) {
-				pageButton.classList.add("page-button-active");
-			}
-			pageButton.textContent = `${i + 1}`;
-			pageButton.dataset.indexNumber = `${i + 1}`;
-			buttonContainer.appendChild(pageButton);
-			pageButton.addEventListener("click", this.switchPage);
-		}
-		options.mainContainer.appendChild(buttonContainer);
-	}
-	switchPage(e: Event) {
-		if ((e.target as Element).classList.contains("page-button-active")) {
-			return;
-		}
-		this.renderData.lastPageClicked = Number((e.target as HTMLElement).dataset.indexNumber);
-		this.renderMainPage(this.renderData);
-	}
-	renderPostWithComments() {}
+  renderPosts(options: ViewRenderData, callback?: Function) {
+    let postContainer = document.createElement("div");
+    postContainer.classList.add("posts-container");
+    for (
+      let i = (options.lastPageClicked - 1) * options.numberOfPostsOnPerPage;
+      i <= options.lastPageClicked * options.numberOfPostsOnPerPage - 1;
+      i++
+    ) {
+      this.renderSinglePost(options.arraysOfPosts[i], postContainer, () => {
+        this.renderPostWithComments(options.arraysOfPosts[i], options);
+      });
+    }
+    options.mainContainer.appendChild(postContainer);
+  }
+  renderPageNumberButtons(options: ViewRenderData) {
+    let buttonContainer = document.createElement("div");
+    buttonContainer.classList.add("page-button-container");
+    for (let i = 0; i < options.numberOfPages; i++) {
+      let pageButton = document.createElement("div");
+      pageButton.classList.add("page-button");
+      if (options.lastPageClicked == i + 1) {
+        pageButton.classList.add("page-button-active");
+      }
+      pageButton.textContent = `${i + 1}`;
+      pageButton.dataset.indexNumber = `${i + 1}`;
+      buttonContainer.appendChild(pageButton);
+      pageButton.addEventListener("click", this.switchPage);
+    }
+    options.mainContainer.appendChild(buttonContainer);
+  }
+  switchPage(e: Event) {
+    if ((e.target as Element).classList.contains("page-button-active")) {
+      return;
+    }
+    this.renderData.lastPageClicked = Number((e.target as HTMLElement).dataset.indexNumber);
+    this.renderMainPage(this.renderData);
+  }
 
-	renderSinglePost(post: Post, container: Element, callback?: Function) {
-		let postElement = document.createElement("div");
-		postElement.classList.add("post");
-		let postElementDescription = document.createElement("h1");
-		postElementDescription.textContent = `Post#${post.id} ${post.title}`;
-		let postElementBody = document.createElement("p");
-		postElementBody.textContent = post.body;
-		postElement.append(postElementDescription, postElementBody);
-		if (callback) {
-			postElement.addEventListener(
-				"click",
-				() => {
-					callback(post);
-				},
-				{ once: true }
-			);
-		}
-		container.appendChild(postElement);
-	}
-	static renderComments() {}
-	static renderError() {}
+  /*Post page with comments********************************************************************************* */
+  async loadComments(post: Post) {
+    try {
+      this.renderData.arraysOfComments = await this.controller.requestToServer(`/posts/${post.id}/comments`);
+      console.log(this.renderData.arraysOfComments.length);
+    } catch (err) {
+      console.log("Error with load comments");
+      console.log(err);
+    }
+  }
+  async renderPostWithComments(post: Post, options: ViewRenderData) {
+    await this.loadComments(post);
+    options.mainContainer.replaceChildren();
+    this.renderFunctionalButton("", this.renderData.mainContainer, () => {
+      this.renderMainPage(options);
+    });
+    /*Creating a personal container */
+    const containerPostAndComments = document.createElement("div");
+    containerPostAndComments.classList.add("post-comments-container");
+    options.mainContainer.append(containerPostAndComments);
+    this.renderSinglePost(post, containerPostAndComments);
+    this.renderComments(options.arraysOfComments, containerPostAndComments);
+  }
+  renderSinglePost(post: Post, container: Element, callback?: Function) {
+    let postElement = document.createElement("div");
+    postElement.classList.add("post");
+    let postElementDescription = document.createElement("h1");
+    postElementDescription.textContent = `Post#${post.id} ${post.title}`;
+    let postElementBody = document.createElement("p");
+    postElementBody.textContent = post.body;
+    postElement.append(postElementDescription, postElementBody);
+    if (callback) {
+      postElement.addEventListener(
+        "click",
+        () => {
+          callback(post);
+        },
+        { once: true }
+      );
+    }
+    container.appendChild(postElement);
+    return postElement;
+  }
+  renderComments(comments: Array<Comments>, container: Element) {
+    if (comments) {
+      for (let i = 0; i <= comments.length - 1; i++) {
+        let commentElement = document.createElement("div");
+        commentElement.classList.add("comment");
+        let commentNumber = document.createElement("h1");
+        commentNumber.textContent = `Comment#${i + 1}`;
+        let commentatorName = document.createElement("h2");
+        commentatorName.textContent = comments[i].name;
+        let commentatorEmail = document.createElement("h2");
+        commentatorEmail.textContent = comments[i].email;
+        let commentText = document.createElement("p");
+        commentText.textContent = comments[i].body;
+        commentElement.append(commentNumber, commentatorEmail, commentText);
+        container.appendChild(commentElement);
+      }
+    }
+  }
+
+  renderError(options: ViewRenderData) {
+    options.mainContainer.replaceChildren();
+    this.renderFunctionalButton("main", options.mainContainer, this.loadDataFromServer);
+    let errorMessage = document.createElement("p");
+    errorMessage.textContent = "Error in request. I am sorry";
+    options.mainContainer.appendChild(errorMessage);
+  }
 }
 const instance = new PostView();
-instance.loadDataFromServer();
+document.querySelector(".request").addEventListener("click", instance.loadDataFromServer);
