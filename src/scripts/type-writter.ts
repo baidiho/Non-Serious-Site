ae246565314298c21ad41286afd5e985c508ffff
Ihor <ygrik10@gmail.com>
2023-03-07 13:30:08 +0200

    Last changes

--- src/scripts/type-writter.ts
+++ src/scripts/type-writter.ts
@@ -2,7 +2,7 @@ export default function typewriterScript() {
   const elementText = document.querySelector(".text-wrapper p");
   const mockText =
     "My name is Ihor. If you are looking this site now, you propably have seen my CV or GitHub account. " +
-    "This isn't serious site. I've tried to show that i know something features of Front-End development. As you can see i am not designer and in some aspects this site doesn`t pretty. Excuse me for this designers pain, that you have felt xD. " +
+    "This isn't serious site. I've tried to show that i know something features of Front-End development. As you can see i am not designer and in some aspects this site doesn`t pretty. Excuse me for this designer pain, that you have felt xD. " +
     "Take me to work, please.";
   let i: number = 0;
   function recursion(): any {
